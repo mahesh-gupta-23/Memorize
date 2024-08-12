b6129747fd71ac1ac10adc503fde09f9fd68e710
Mahesh Gupta <maheshgupta2329@gmail.com>
2024-08-12 15:35:09 +0530

    Setup CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View{
+        if(isFaceUp){
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("Hello, world! 🫡")
+                    .multilineTextAlignment(.center)
+            })
+        }
+        else{
+            RoundedRectangle(cornerRadius: 12)
+        }
     }
 }
 
