06c50732ce79c0737492ddb46def8791896866db
Mahesh Gupta <mahesh.gupta@gojek.com>
2024-08-12 15:14:27 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mahesh Gupta on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
