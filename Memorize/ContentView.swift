880051a5399ad628c6e2aef2956372cdc64a7c1c
Mahesh Gupta <maheshgupta2329@gmail.com>
2024-09-17 16:13:07 +0530

    Add Button to add and remove card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,31 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["😭","🫢","🐶","🐥","🌈","🐚","👻","🎃","👽","🤖"]
+    
+    @State var cardCount:Int = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardAdjuster
+        }
+        .padding()
+    }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3 , contentMode: .fit)
+            }
         }
         .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-        .padding()
+    }
+    
+    var cardAdjuster: some View{
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cardAdder: some View{
+        cardCountAdjuster(by: +1, icon: "rectangle.stack.badge.plus.fill")
+    }
+    
+    var cardRemover: some View{
+        cardCountAdjuster(by: -1, icon: "rectangle.stack.badge.minus.fill")
+    }
+    
+    func cardCountAdjuster(by offset: Int, icon: String) -> some View{
+        Button {
+            cardCount += offset
+        } label: {
+            Image(systemName: icon)
+        }
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
+    var content: String
     
     var body: some View{
-        if(isFaceUp){
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
-                Text("Hello, world! 🫡")
-                    .multilineTextAlignment(.center)
-            })
-        }
-        else{
-            RoundedRectangle(cornerRadius: 12)
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
